58edfc394f79e8a08cccdd29e35eb27438a01efa
sanupnair <93447500+sanupnair@users.noreply.github.com>
2021-11-26 00:42:07 +0530

    updated

--- JS/todo.js
+++ JS/todo.js
@@ -18,30 +18,3 @@ function validate(callBack) {
 validate(callBack, load);
 
 
-// Change the background colour when checkbox is checked
-
-function change(obj) {
-
-var tr=obj.parentNode.parentNode; 
-tr.style.backgroundColor=(obj.checked)? '#3ccf68' : '#3ccf68';
-
-}
-
-// Alert message on marking task as completed
-function checkboxes(){
-var inputs = document.getElementsByTagName("input");
-var inputObj;
-var count = 0;
-for(var count1 = 0;count1<inputs.length;count1++) {
-inputObj = inputs[count1];
-var type = inputObj.getAttribute("type");
-if (type == 'checkbox' && inputObj.checked) {
-  count++;
-}
-}
-alert ("Congrats  " + count + "  Tasks have been Succesfully Completed")  
-document.getElementsByTagName("input").checked = true;
-document.getElementsByTagName("input").checked = false;
-
-}
-
