b1dd5e5f914a183ae47d52070fa05ca6caf84b1b
sanupnair <93447500+sanupnair@users.noreply.github.com>
2021-11-25 16:47:35 +0530

    updated js

--- JS/todo.js
+++ JS/todo.js
@@ -1,25 +1,33 @@
-function validate(show) {
+// Validation and load page - JS callback function
+
+function alertmsg(load) {
+
+  window.location.href = "main.html";
+}
+function validate(callBack) {
     var user = document.getElementById("userid").value;
     var pass = document.getElementById("pass").value;
-
     if (user == "Admin" && pass == "12345"){
-    window.location.href = "main.html";
-    }
-    else {
-        alert("Incorrect Username or Password \n Username = Admin \nPassword = 12345")
+    
+      }
+      else {
+        alert("Incorrect Username or Password");
+        callBack();
     }
 }
+validate(callBack, load);
 
 
-function change(obj) 
+// Change the background colour when checkbox is checked
 
-{
+function change(obj) {
+
 var tr=obj.parentNode.parentNode; 
 tr.style.backgroundColor=(obj.checked)? '#3ccf68' : '#3ccf68';
 
-
 }
 
+// Alert message on marking task as completed
 function checkboxes(){
 var inputs = document.getElementsByTagName("input");
 var inputObj;
@@ -31,8 +39,9 @@ if (type == 'checkbox' && inputObj.checked) {
   count++;
 }
 }
-
 alert ("Congrats  " + count + "  Tasks have been Succesfully Completed")  
 document.getElementsByTagName("input").checked = true;
-document.getElementsByTagName("input").checked = false;}
+document.getElementsByTagName("input").checked = false;
+
+}
 
